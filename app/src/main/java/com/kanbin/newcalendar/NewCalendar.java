c68f7c858f794f57af0be22e016777ca7ec608e6
kanbin <kanbin@live.com>
2017-08-23 13:34:33 +0800

    事件（交互）业务逻辑

--- app/src/main/java/com/kanbin/newcalendar/NewCalendar.java
+++ app/src/main/java/com/kanbin/newcalendar/NewCalendar.java
@@ -1,24 +1,126 @@
 package com.kanbin.newcalendar;
 
 import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by SONY on 2017/8/23.
  */
 
 public class NewCalendar extends LinearLayout {
+
+    private ImageView btnPrev;
+    private ImageView btnNext;
+    private TextView txtDate;
+    private GridView gridView;
+
+    private Calendar curDate = Calendar.getInstance();
+    /**
+     * 构造方法：初始化时的入口
+     * @param context
+     */
     public NewCalendar(Context context) {
         super(context);
     }
 
     public NewCalendar(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initControl(context);
     }
 
     public NewCalendar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initControl(context);
+    }
+
+    private void initControl(Context context){
+        bindControl(context);
+        bindControlEvent();
+        renderCalendar();
+    }
+
+    private void bindControl(Context context) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.calendar_view, this);
+        btnPrev = (ImageView) findViewById(R.id.btnPrev);
+        btnNext = (ImageView) findViewById(R.id.btnNext);
+        txtDate = (TextView) findViewById(R.id.txtDate);
+        gridView = (GridView) findViewById(R.id.calendar_grid);
+    }
+
+    private void bindControlEvent() {
+        btnPrev.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curDate.add(Calendar.MONTH, -1);
+                renderCalendar();   // 渲染
+            }
+        });
+
+        btnNext.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curDate.add(Calendar.MONTH, 1);
+                renderCalendar();
+            }
+        });
+    }
+
+    private void renderCalendar() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM yyyy");
+        txtDate.setText(simpleDateFormat.format(curDate.getTime()));
+
+        ArrayList<Date> cells = new ArrayList<>();
+        Calendar calendar = (Calendar) curDate.clone();
+
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        int prevDays = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+        calendar.add(Calendar.DAY_OF_MONTH, -prevDays);
+
+        int maxCellCount = 6 * 7;
+        while (cells.size() < maxCellCount){
+            cells.add(calendar.getTime());
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        gridView.setAdapter(new CalendarAdapter(getContext(), cells));
+    }
+
+    private class CalendarAdapter extends ArrayAdapter<Date>{
+        LayoutInflater inflater;
+
+        public CalendarAdapter(@NonNull Context context, ArrayList<Date> days) {
+            super(context, R.layout.calendar_text_day, days);
+            inflater = LayoutInflater.from(context);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            Date date = getItem(position);
+            if (convertView == null){
+                convertView = inflater.inflate(R.layout.calendar_text_day, parent, false);
+            }
+
+            int day = date.getDate();
+            ((TextView) convertView).setText(String.valueOf(day));
+
+            return convertView;
+        }
     }
 }
