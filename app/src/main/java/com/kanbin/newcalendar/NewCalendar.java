5efe36697e8f5a5a4cbe338c9e6b2bac6f9d34c6
kanbin <kanbin@live.com>
2017-08-23 12:57:18 +0800

    UI

---
+++ app/src/main/java/com/kanbin/newcalendar/NewCalendar.java
@@ -0,0 +1,24 @@
+package com.kanbin.newcalendar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by SONY on 2017/8/23.
+ */
+
+public class NewCalendar extends LinearLayout {
+    public NewCalendar(Context context) {
+        super(context);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
